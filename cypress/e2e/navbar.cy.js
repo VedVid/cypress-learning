99cd0122180a62291c8582620c6c3ad20797deb7
Tomasz Nowakowski <toalak@gmail.com>
2024-07-21 09:58:16 -0400

    Added test for checking Home, Games, Other, Contact buttons from the navbar

--- cypress/e2e/navbar.cy.js
+++ cypress/e2e/navbar.cy.js
@@ -13,13 +13,43 @@ describe('Checks navbar functionality', () => {
     })
   })
 
-  it.only('Checks if there are buttons for "Home", "Games", "Other projects" and "Contact" pages', () => {
-    pages.forEach((item, _) => {
-      cy.visit(`https://localhost${item}`);
-      cy.get('.navbar').find('a').contains('Home').should('have.attr', 'href');
-      cy.get('.navbar').find('a').contains('Games').should('have.attr', 'href');
-      cy.get('.navbar').find('a').contains('Other').should('have.attr', 'href');
-      cy.get('.navbar').find('a').contains('Contact').should('have.attr', 'href');
+  describe.only('Checks if every page has "Home", "Games", "Other" and "Contact" buttons with correct links', () => {
+    // Home page uses different style of links than the other pages.
+    // It links pages directly, while the other pages first go to the parent directory,
+    // and only from there navigate to the other pages.
+    it('Tests home page', () => {
+      cy.visit("https://localhost");
+      cy.get('.navbar').find('a').contains('Home').should('have.attr', 'href', '.');
+      cy.get('.navbar').find('a').contains('Games').should('have.attr', 'href', 'games/');
+      cy.get('.navbar').find('a').contains('Other').should('have.attr', 'href', 'other/');
+      cy.get('.navbar').find('a').contains('Contact').should('have.attr', 'href', 'contact/');
+    })
+  
+    it('Tests all pages with expection for home page', () => {
+      //All pages with exception for Home page have links that cause going to the 
+      //parent directory first, like that: `href="../games".
+      pages.forEach((item, _) => {
+        if (item === '/') {
+          return
+        }
+        cy.visit(`https://localhost${item}`)
+        cy.get('.navbar').find('a').contains('Home').should('have.attr', 'href', '..')
+        if (item === "/games/") {
+          cy.get('.navbar').find('a').contains('Games').should('have.attr', 'href', './')
+        } else {
+          cy.get('.navbar').find('a').contains('Games').should('have.attr', 'href', '../games/');
+        }
+        if (item === "/other/") {
+          cy.get('.navbar').find('a').contains('Other').should('have.attr', 'href', './')
+        } else {
+          cy.get('.navbar').find('a').contains('Other').should('have.attr', 'href', '../other/')
+        }
+        if (item === '/contact/') {
+          cy.get('.navbar').find('a').contains('Contact').should('have.attr', 'href', './')
+        } else {
+          cy.get('.navbar').find('a').contains('Contact').should('have.attr', 'href', '../contact/')
+        }
+      })
     })
   })
 })
