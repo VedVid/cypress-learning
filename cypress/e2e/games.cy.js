9f14f42fcbf57f56ef14caf3443e5d541cccb8b1
Tomasz Nowakowski <toalak@gmail.com>
2024-07-22 14:06:18 -0400

    Added test for checking for active element in Game page navbar

--- cypress/e2e/games.cy.js
+++ cypress/e2e/games.cy.js
@@ -1,6 +1,13 @@
 describe('Checks content of Game page', () => {
-  it('Checks if every sidebar link is valid', () => {
+  beforeEach(() => {
     cy.visit('https://localhost/games/')
+  })
+
+  it('"Games" button should be marked as active', () => {
+    cy.get('.navbar').get('.active').contains('Games').should('exist')
+  })
+
+  it('Checks if every sidebar link is valid', () => {
     cy.get('.bs-sidebar > .nav').find('a').each((item) => {
       cy.visit(`https://localhost/games/${item.attr('href')}`)
     })
